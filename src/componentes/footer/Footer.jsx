fcf906d2df7cc079af3764b091335e08e8f45228
P3dro-Lucas <pedrolucasduarted.silva@gmail.com>
2024-01-04 16:23:16 -0300

    2

--- src/componentes/footer/Footer.jsx
+++ src/componentes/footer/Footer.jsx
@@ -11,11 +11,11 @@ const Footer = () => {
 
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#service">Services</a></li>
+        <li><a href="#about">Sobre-mim</a></li>
+        <li><a href="#experience">Experiencia</a></li>
+        <li><a href="#service">Serviços</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="#contact">Contacte-me</a></li>
       </ul>
 
       <div className="footer__socials">
